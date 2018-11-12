9d68e8694e7482437147fd07d92a0d200fee2128
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2018-11-12 20:29:00 +0100

    Assign DIALOG_INFO_TERMINATED as default state if dialog was not found
    FreeSWITCH interoperability: after subscribing there is no "dialog" element if extension is idle
    (which actually makes sense)
    => assuming that extension is in "terminated" state by default.

--- baresip/include/baresip_dialog_info_status.h
+++ baresip/include/baresip_dialog_info_status.h
@@ -2,7 +2,7 @@
 #define BARESIP_DIALOG_INFO_STATUS
 
 enum dialog_info_status {
-	DIALOG_INFO_UNKNOWN,		///< not subscribed / subscription failed
+	DIALOG_INFO_UNKNOWN = 0,		///< not subscribed / subscription failed
 	DIALOG_INFO_TERMINATED,
 	DIALOG_INFO_EARLY,
 	DIALOG_INFO_CONFIRMED
--- baresip/modules/dialog_info/dialog_info_subscriber.c
+++ baresip/modules/dialog_info/dialog_info_subscriber.c
@@ -78,6 +78,7 @@ struct dialog_info_context {
 	enum dialog_info_direction direction;	
 	char identity_display[64];
 	char identity[64];
+	enum dialog_info_status status;	
 };
 
 static int dialog_info_sxmlc_callback(XMLEvent evt, const XMLNode* node, SXML_CHAR* text, const int n, SAX_Data* sd)
@@ -87,8 +88,13 @@ static int dialog_info_sxmlc_callback(XMLEvent evt, const XMLNode* node, SXML_CH
 	switch(evt) {
 		case XML_EVENT_START_NODE:
 			if (!ctx->in_dialog_info) {
-				if (!stricmp(node->tag, C2SX("dialog-info")))
+				if (!stricmp(node->tag, C2SX("dialog-info"))) {
 					ctx->in_dialog_info = true;
+					// FreeSWITCH interoperability: after subscribing there is no "dialog" element if extension is idle
+					// (which actually makes sense)
+					// => assuming that extension is in "terminated" state by default
+					ctx->status = DIALOG_INFO_TERMINATED;
+				}
 			}
 			if (!ctx->in_dialog_info)
 				break;
@@ -226,7 +232,7 @@ static void notify_handler(struct sip *sip, const struct sip_msg *msg,
 	
     */
 
-	/** \todo More precise matching for dialog-info body */
+	/** \todo This should be replaced with newer code using proper XML parsing */
 	if (!re_regex((const char *)mbuf_buf(msg->mb), mbuf_get_left(msg->mb), STR_EARLY)) {
 		status = DIALOG_INFO_EARLY;
 	} else if (!re_regex((const char *)mbuf_buf(msg->mb), mbuf_get_left(msg->mb), STR_CONFIRMED)) {
@@ -238,6 +244,7 @@ static void notify_handler(struct sip *sip, const struct sip_msg *msg,
 	}
 
 	memset(&ctx, 0, sizeof(ctx));
+	ctx.status = DIALOG_INFO_UNKNOWN;
 	SAX_Callbacks_init(&sax);
 	sax.all_event = dialog_info_sxmlc_callback;
 
@@ -246,6 +253,7 @@ static void notify_handler(struct sip *sip, const struct sip_msg *msg,
 #else
 	// test
 	{
+	#if 1
 		const char* body =
 			"\t<?xml version=\"1.0\"?>\n"
 			"\t<dialog-info xmlns=\"urn:ietf:params:xml:ns:dialog-info\" version=\"12\" state=\"full\" entity=\"sip:11@pbx.xxxx.net\">\n"
@@ -264,6 +272,9 @@ static void notify_handler(struct sip *sip, const struct sip_msg *msg,
 			"\t\t</dialog>\n"
 			"\t</dialog-info>"
 			;
+	#else
+		const char* body = "<dialog-info xmlns=\"urn:ietf:params:xml:ns:dialog-info\" version=\"0\" state=\"full\" entity=\"sip:12@pbx.xxx.net\"></dialog-info>";
+	#endif
 		XMLDoc_parse_buffer_SAX_len(body, strlen(body), "root", &sax, &ctx);
 	}
 #endif
@@ -271,6 +282,10 @@ static void notify_handler(struct sip *sip, const struct sip_msg *msg,
 
 	pl_set_str(&pl_remote_identity, ctx.identity);
 	pl_set_str(&pl_remote_identity_display, ctx.identity_display);
+	if (status == DIALOG_INFO_UNKNOWN)
+	{
+        status = ctx.status;
+    }
 	contact_set_dialog_info(dlg_info->contact, status, ctx.direction, &pl_remote_identity, &pl_remote_identity_display);
 }
 
