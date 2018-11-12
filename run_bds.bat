612a02233cc5fd4267b8a21a54137cb51f8094e9
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2018-11-12 13:43:58 +0100

    Fix typo in .bat starting TC++ on Windows 7

--- run_bds.bat
+++ run_bds.bat
@@ -1,4 +1,6 @@
-del %temp%\EditorLineEnds*.tt 
+REM Handling weird issue with BDS2006 and Windows 7
+
+del %temp%\EditorLineEnds*.ttr
 set dt=%date%_%time:~3,2%-%time:~6,2%
 rename "%temp%\EditorLineEnds.ttr" "EditorLineEnds_%dt%.ttr"
 start "" "C:\Program Files\Borland\BDS\4.0\Bin\bds.exe"
