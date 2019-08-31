6647aeefb169a0efc70e61c769b2c2cfbd90998e
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2019-08-31 23:33:35 +0200

    common.lib: remove vcl.h from precompiled headers

--- common/common.cpp
+++ common/common.cpp
@@ -1,7 +1,8 @@
 //---------------------------------------------------------------------------
-
+#if 0
 #include <vcl.h>
 #pragma hdrstop
+#endif
 #define Library
 
 // To add a file to the library use the Project menu 'Add to Project'.
