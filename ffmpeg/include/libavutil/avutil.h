682de71981813be47441d542cd51e8ffcdb3ce13
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2023-03-20 20:11:26 +0100

    Add ULL sufix to AV_NOPTS_VALUE

--- ffmpeg/include/libavutil/avutil.h
+++ ffmpeg/include/libavutil/avutil.h
@@ -245,7 +245,7 @@ const char *av_get_media_type_string(enum AVMediaType media_type);
  * either pts or dts.
  */
 
-#define AV_NOPTS_VALUE          ((int64_t)UINT64_C(0x8000000000000000))
+#define AV_NOPTS_VALUE          ((int64_t)UINT64_C(0x8000000000000000ULL))
 
 /**
  * Internal time base represented as integer
